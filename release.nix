d3245f0e3e74be8d735f404f3bf907e356564167
Gabriel Gonzalez <Gabriel439@gmail.com>
2018-01-28 07:53:06 -0800

    Add Nix derivation to build project

--- release.nix
+++ release.nix
@@ -1,13 +1,3 @@
-# You can build this repository using Nix by running:
-#
-#     $ nix-build -A pipes-group release.nix
-#
-# You can also open up this repository inside of a Nix shell by running:
-#
-#     $ nix-shell -A pipes-group.env release.nix
-#
-# ... and then Nix will supply the correct Haskell development environment for
-# you
 let
   config = {
     packageOverrides = pkgs: {
@@ -23,5 +13,5 @@ let
     import <nixpkgs> { inherit config; };
 
 in
-  { pipes-group = pkgs.haskellPackages.pipes-group;
+  { inherit (pkgs.haskellPackages) pipes-group;
   }
