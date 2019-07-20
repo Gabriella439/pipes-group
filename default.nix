9c8489f0d35379b0f6ad9543d263e81925184232
Gabriel Gonzalez <Gabriel439@gmail.com>
2019-07-19 20:19:28 -0700

    Support `lens-family-core-2.0.0`
    Related to https://github.com/commercialhaskell/stackage/issues/4725

--- default.nix
+++ default.nix
@@ -1,12 +1,9 @@
 let
   fetchNixpkgs = import ./nix/fetchNixpkgs.nix;
 
-  nixpkgs = fetchNixpkgs {
-    rev = "804060ff9a79ceb0925fe9ef79ddbf564a225d47";
-
-    sha256 = "01pb6p07xawi60kshsxxq1bzn8a0y4s5jjqvhkwps4f5xjmmwav3";
-
-    outputSha256 = "0ga345hgw6v2kzyhvf5kw96hf60mx5pbd9c4qj5q4nan4lr7nkxn";
+  nixpkgs = builtins.fetchTarball {
+    url    = "https://github.com/NixOS/nixpkgs/archive/3e5cf4fa63552795ed7fa6ee160de4b3b1ef5753.tar.gz";
+    sha256 = "0a60l2jrplgfrfhw40cyr73nksb2bdjl0kxgs6i7nqilxfdd3kb6";
   };
 
   readDirectory = import ./nix/readDirectory.nix;
